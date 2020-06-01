bfae4c2ac39b89cc103c1e283e8f26f724504b8b
Onowu Christopher <totiruzi@yahoo.com>
2020-06-02 02:49:37 +0300

    Create searchbar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = { term: '' };
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+  render() {
+    return (
+      <div onSubmit={this.onFormSubmit} className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Search Car</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+export default SearchBar;
